b76e67c815967767fb64ea821ba62633d63bf45f
Akshit Jain <94056853+akshitjain2004@users.noreply.github.com>
2023-06-24 12:58:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let sections  = document.querySelectorAll('section');
+let navLinks  = document.querySelectorAll('header nav a');
+
+
+window.onscroll= () => {
+sections.array.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+});
+
+};
